e3b5d48e629c9fcf775cf5c122058f2ad14a01be
Thiago Felipe <thiagof.g3@gmail.com>
2024-05-17 18:46:02 -0300

    test: verificando se o arquivo foi de fato salvo

--- tests/Feature/UploadControllerTest.php
+++ tests/Feature/UploadControllerTest.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Uploadfile;
 use App\Jobs\ProcessFilesJob;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class UploadControllerTest extends TestCase
@@ -21,5 +22,19 @@ class UploadControllerTest extends TestCase
         $uploadController->upload($request);
 
         Queue::assertPushed(ProcessFilesJob::class);
+    }
+
+    public function testShouldCallStorage(): void
+    {
+        Storage::fake('local');
+
+        $uploadController = new UploadController();
+        $request = new Uploadfile();
+        $file = UploadedFile::fake()->create('index.csv');
+		$request->files->set('file', $file);
+        $uploadController->upload($request);
+
+        Storage::disk('local')->assertExists('files/'.$file->hashName());
+        Storage::disk('local')->delete('files/'.$file->hashName());
     }
 }
